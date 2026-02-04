71405d5d44b8fc34d30b3634330e36b10efbddc8
amalujose189 <amalujose189@gmail.com>
2026-02-04 17:37:19 +0530

    04-02-2026

--- mypractise.py
+++ mypractise.py
@@ -1,4 +1,4 @@
-list=[1,2,3,4,5,6,2,7,3,8,10,2]
+'''  list=[1,2,3,4,5,6,2,7,3,8,10,2]
 unique1 = []
 repeating=[]
 def remove_duplicates(input_list):
@@ -11,5 +11,11 @@ def remove_duplicates(input_list):
                repeating.append(i)
 remove_duplicates(list)
 print(unique1)
-print(repeating)
+print(repeating)'''
 
+lst=[2,3,4,5,4,2,3,3,3]
+dict1={}
+for i in lst:
+    if lst.count(i)>1:
+        dict1[i]=lst.count(i)
+ 
