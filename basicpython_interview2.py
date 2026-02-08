897d7dd852a35f4e0ed4c2763e7d257ad3bffd09
amalujose189 <amalujose189@gmail.com>
2026-02-08 16:22:53 +0530

    06-02-2026

--- basicpython_interview2.py
+++ basicpython_interview2.py
@@ -17,3 +17,63 @@ sorted_dict = dict(sorted(dict1.items(), key=lambda x: x[1]))
 
 print(sorted_dict)
 print("1-------------------------------------------------------")
+'''2️⃣ Find key with maximum value
+d = {'math': 90, 'science': 85, 'english': 88}
+
+
+➡ Output: 'math' '''
+marks = {'math': 900, 'science': 85, 'english': 88,'Hindi':100}
+key2=max(marks, key=marks.get)
+print(key2)
+
+'''🔍 Working
+
+max() normally returns the maximum element
+
+Here, marks is a dictionary → iteration happens over keys
+
+key=marks.get tells Python:
+
+“Compare keys based on their values”
+
+So comparisons happen like this:
+
+math → 900
+science → 85
+english → 88
+Hindi → 100
+
+
+✔ Highest value = 900
+✔ Key = 'math'
+
+✅ Output
+math
+
+eg1:
+d = {'a': 5, 'b': 10, 'c': 3}
+print(max(d))
+Output
+c
+eg2:
+max() with Dictionary Values (Using key)
+d = {'a': 5, 'b': 10, 'c': 3}
+print(max(d, key=d.get))
+
+Output
+b
+eg3:
+data = [('math', 90), ('science', 85), ('english', 88)]
+print(max(data, key=lambda x: x[1]))
+Output
+('math', 90)
+
+eg4:
+words = ["apple", "banana", "cherry"]
+print(max(words))
+Output
+cherry
+
+'''
+
+
